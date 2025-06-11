2153cc79ee3774fa2e8dfdae82a627cf199e679a
komalneharkar <komalneharkar987@gmail.com>
2025-06-11 16:19:22 +0530

    done

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load the saved regression model
+model = joblib.load('regression_model.joblib')
+
+# streamlit app UI
+st.title("job Package Prediction Based on CGPA")
+st.write("Enter your CGPA to predict the expected job package:")
+
+# user input for CGPA
+cgpa = st.number_input("CGPA", min_value=0.0, max_value=10.0, step=0.1)
+
+# Predict button
+if st.button("Predict Package"):
+    # Prepare input data for the model
+    input_data = np.array([[cgpa]])
+
+    # Predict the Package
+    prediction = model.predict(input_data)
+    predicted_value = float(prediction[0]) # convert Numpy value to float
+
+    # show the result
+    st.success(f"Predicted Package: ₹{predicted_value:,.2f} LPA")
+    
+    
+
+
+
